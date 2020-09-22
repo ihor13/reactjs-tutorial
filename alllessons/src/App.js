bb00db4c5bd5443091a42f095297d3226c1603d9
ihor13 <igor.gavrilchuk.3@mail.ru>
2020-09-23 01:37:50 +0300

    Lesson 11 react samurai

--- alllessons/src/App.js
+++ alllessons/src/App.js
@@ -4,21 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://www.nicepng.com/png/detail/83-838746_batman-arkham-knight-logo-background-logo-background-batman.png"/>
+      </header> 
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+        <a>Messages</a>
+        </div>
+        <div>
+        <a>News</a>
+        </div>
+        <div>
+        <a>Music</a>
+        </div>
+        <div>
+        <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg" />
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>New post</div>
+        </div>
+        <div>
+          <div>
+            post 1 
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
